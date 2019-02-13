118f1d8820b86d8c7671ab0157b441422003b4ba
David Sepulveda <david.sepulvedamarti@ucalgary.ca>
2019-02-13 11:34:52 -0700

    Update webScripts.js

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -22,7 +22,7 @@ else {
 	      document.write("test0:"+ "<br>");
 
 	      document.write("test5:" + myObj["activities-steps"] + "<br>");
-	      document.write("test5:" + myObj["activities-steps"][0] + "<br>");
+	      document.write("test6:" + myObj["activities-steps"][0] + "<br>");
 
 	      //document.write("test2:" + myArray.activities-heart + "<br>");
 
