0aa54fbefb9cf6abbb029f340153a0889a1d9e6c
David Sepulveda <david.sepulvedamarti@ucalgary.ca>
2019-02-13 09:26:44 -0700

    Update webScripts.js

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -7,6 +7,16 @@ else {
 
 	var userId = url.split("#")[1].split("=")[2].split("&")[0];
 
-	console.log(access_token);
-	console.log(userId);
+	//console.log(access_token);
+	//console.log(userId);
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'httpss://api.fitbit.com/1/user/'+ userId +'/activities/heart/date/today/1w.json');
+	xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+	xhr.onload = function() {
+	   if (xhr.status === 200) {
+	      console.log(xhr.responseText);
+	      document.write(xhr.responseText);
+	   }
+	};
+	xhr.send();
 }
